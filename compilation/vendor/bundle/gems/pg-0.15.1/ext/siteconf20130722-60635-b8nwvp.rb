600272dc43f3775dc8f6fc5c57db759961657429
Nick Gooding <vballnickg001@gmail.com>
2013-07-22 11:23:01 -0700

    removing vendor bundle

--- compilation/vendor/bundle/gems/pg-0.15.1/ext/siteconf20130722-60635-b8nwvp.rb
+++
@@ -1,6 +0,0 @@
-require 'rbconfig'
-dest_path = "./.gem.20130722-60635-1f0892d"
-RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
-RbConfig::CONFIG['sitearchdir'] = dest_path
-RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
-RbConfig::CONFIG['sitelibdir'] = dest_path
